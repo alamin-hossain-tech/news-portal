da44bc24fa5fcdd1b54363b5d69cb2f1d9eba4a0
Al-Amin Hossain <alaminnishat@gmail.com>
2022-09-04 10:52:53 +0600

    News Display Section Added.

--- scripts/main.js
+++ scripts/main.js
@@ -27,93 +27,93 @@ let displayCategory = (categoriesArray) => {
 
 // ----------------------------------
 
-// let displayNews = (news, categoryName) => {
-//   let totalItem = document.getElementById("totalNews");
-//   let articles = document.getElementById("artcles");
-//   articles.innerHTML = "";
+let displayNews = (news, categoryName) => {
+  let totalItem = document.getElementById("totalNews");
+  let articles = document.getElementById("artcles");
+  articles.innerHTML = "";
 
-//   let newarray = news.sort((a, b) =>
-//     b.total_view > a.total_view ? 1 : b.total_view < a.total_view ? -1 : 0
-//   );
-//   console.log(newarray);
-//   newarray.forEach((post) => {
-//     totalItem.innerText = `${
-//       news.length != 0 ? news.length : "No data Found"
-//     } items found for category ${categoryName}`;
+  let newarray = news.sort((a, b) =>
+    b.total_view > a.total_view ? 1 : b.total_view < a.total_view ? -1 : 0
+  );
 
-//     let article = document.createElement("div");
-//     article.classList.add("row");
-//     article.innerHTML = `
-//       <article  class="shadow p-4 mb-5 bg-body rounded">
-//       <div class="row">
-//         <div class="col-12 col-sm-12 col-md-12 col-lg-4">
-//           <div class="thumbnail w-100 h-100">
-//             <img
-//               src="${post.image_url}"
-//               style="width: 100%; height: 100%"
-//               alt="image"
-//               class="rounded"
-//             />
-//           </div>
-//         </div>
-//         <div class="col-12 mt-sm-4 col-sm-12 col-md-12 col-lg-8 px-4">
-//           <h2>
-//             ${post.title}
-//           </h2>
-//           <div class="text mt-3 mb-4">
-//             <p class="txt_formate">${post.details.slice(0, 400)}......</p>
-//           </div>
-//           <!-- post information -->
-//           <div class="post_details row mb-3">
-//             <div
-//               class="col d-flex align-items-center justify-content-between"
-//             >
-//               <!-- Author -->
-//               <div class=" d-none d-sm-none d-md-block  author d-flex align-items-center gap-2">
-//                 <img
-//                   style="height: 3rem"
-//                   src=${post.author.img}
-//                   class="img-fluid rounded-circle"
-//                   alt="profile"
-//                 />
-//                 <div class="">
-//                   <p class="border-0 m-0 text-muted">${post.author.name}</p>
-//                   <span class="text-muted">${post.author.published_date} </span>
-//                 </div>
-//               </div>
-//               <!-- Total View -->
-//               <div class="total_view d-none d-sm-none d-md-block">
-//                 <span><i class="fa-regular fa-eye"></i></span>
-//                 <span>${post.total_view}</span>
-//               </div>
-//               <!-- Rating -->
-//               <div class="rating d-none d-sm-none d-md-block">
-//                 <a href="#"
-//                   ><i class="fa-solid fa-star-half-stroke"></i
-//                 ></a>
-//                 <a href="#"><i class="fa-regular fa-star"></i></a>
-//                 <a href="#"><i class="fa-regular fa-star"></i></a>
-//                 <a href="#"><i class="fa-regular fa-star"></i></a>
-//                 <a href="#"><i class="fa-regular fa-star"></i></a>
-//               </div>
-//               <!-- read more -->
-//               <div class="readMore">
-//                 <button data-bs-toggle="modal" data-bs-target="#staticBackdrop" id="showmodal" onclick="showModal('${
-//                   post._id
-//                 }')"
-//                   ><i class="fa-solid fa-angles-right"></i></button>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </article>
+  newarray.forEach((post) => {
+    totalItem.innerText = `${
+      news.length != 0 ? news.length : "No data Found"
+    } items found for category ${categoryName}`;
 
-//       `;
-//     articles.appendChild(article);
-//   });
-//   spinner.classList.add("d-none");
-// };
+    let article = document.createElement("div");
+    article.classList.add("row");
+    article.innerHTML = `
+      <article  class="shadow p-4 mb-5 bg-body rounded">
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-4">
+          <div class="thumbnail w-100 h-100">
+            <img
+              src="${post.image_url}"
+              style="width: 100%; height: 100%"
+              alt="image"
+              class="rounded"
+            />
+          </div>
+        </div>
+        <div class="col-12 mt-sm-4 col-sm-12 col-md-12 col-lg-8 px-4">
+          <h2>
+            ${post.title}
+          </h2>
+          <div class="text mt-3 mb-4">
+            <p class="txt_formate">${post.details.slice(0, 400)}......</p>
+          </div>
+          <!-- post information -->
+          <div class="post_details row mb-3">
+            <div
+              class="col d-flex align-items-center justify-content-between"
+            >
+              <!-- Author -->
+              <div class=" d-none d-sm-none d-md-block  author d-flex align-items-center gap-2">
+                <img
+                  style="height: 3rem"
+                  src=${post.author.img}
+                  class="img-fluid rounded-circle"
+                  alt="profile"
+                />
+                <div class="">
+                  <p class="border-0 m-0 text-muted">${post.author.name}</p>
+                  <span class="text-muted">${post.author.published_date} </span>
+                </div>
+              </div>
+              <!-- Total View -->
+              <div class="total_view d-none d-sm-none d-md-block">
+                <span><i class="fa-regular fa-eye"></i></span>
+                <span>${post.total_view}</span>
+              </div>
+              <!-- Rating -->
+              <div class="rating d-none d-sm-none d-md-block">
+                <a href="#"
+                  ><i class="fa-solid fa-star-half-stroke"></i
+                ></a>
+                <a href="#"><i class="fa-regular fa-star"></i></a>
+                <a href="#"><i class="fa-regular fa-star"></i></a>
+                <a href="#"><i class="fa-regular fa-star"></i></a>
+                <a href="#"><i class="fa-regular fa-star"></i></a>
+              </div>
+              <!-- read more -->
+              <div class="readMore">
+                <button data-bs-toggle="modal" data-bs-target="#staticBackdrop" id="showmodal" onclick="showModal('${
+                  post._id
+                }')"
+                  ><i class="fa-solid fa-angles-right"></i></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+
+      `;
+    articles.appendChild(article);
+  });
+  spinner.classList.add("d-none");
+};
 
 //
 //
@@ -137,4 +137,3 @@ let showNews = (categoryType, cName = "") => {
 //
 //
 fetchCategories();
-console.log("Hello");
